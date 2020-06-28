5056a45a673b7f5d6018ec6115bfaf82510c3204
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-06-28 19:13:39 +0100

    added Static and Media configuration

--- youtube_clone/settings.py
+++ youtube_clone/settings.py
@@ -129,3 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+# Media configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
