77e117e6d06c350b06acf4d2916a7f8f64fd338e
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-06-29 12:58:26 +0100

    Create Playlist model

--- Main/models.py
+++ Main/models.py
@@ -31,3 +31,24 @@ class Video(models.Model) :
             models.UniqueConstraint(fields=['channel', 'title'], name='unique_videos_per_channel')
         ]
 
+    def add_view(self) :
+        self.views = self.views + 1 
+
+class Playlist(models.Model) :
+    id = models.TextField(primary_key=True, max_length=35, default=generetePlaylistId, editable=False)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    is_public = models.BooleanField(default=True)
+    videos = models.ManyToManyField(Video)
+    views = models.IntegerField(default=0)
+    # Dates 
+    created_date = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_date = models.DateTimeField(auto_now=True)    
+
+    class Meta :
+        constraints = [
+            models.UniqueConstraint(fields=['creator', 'title'], name='unique_playlist_per_user')
+        ]
+
+    def add_view(self) :
+        self.views = self.views + 1 
