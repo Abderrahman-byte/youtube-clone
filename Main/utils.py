be49213fca0c093bdf42a140b68ca2240f36addf
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-06-29 16:03:21 +0100

    generate Thumbnail for a video

--- Main/utils.py
+++ Main/utils.py
@@ -1,5 +1,6 @@
-import string
-import random
+from django.conf import settings
+
+import os, string, random, subprocess
 
 def generateId(length=11) :
     chars = string.ascii_letters + string.digits
@@ -8,3 +9,13 @@ def generateId(length=11) :
 def generetePlaylistId(length=35) :
     chars = string.ascii_letters + string.digits
     return ''.join([random.choice(chars) for _ in range(length)])
+
+def generateThumbnail(video_name) :
+    MEDIA_ROOT = settings.MEDIA_ROOT
+    video_path = os.path.join(MEDIA_ROOT, video_name)
+    image_id = generateId(30)
+    image_path = os.path.join(MEDIA_ROOT, f'images/thumbnails/{image_id}.jpg')
+
+    subprocess.call(['ffmpeg', '-i', video_path, '-r', '1/1440', image_path, "-y"])
+
+    return f'/media/images/thumbnails/{image_id}.jpg'
