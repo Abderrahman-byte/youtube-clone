d69f112aa760c296fc3f396b7b132b9c33f77dc6
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-06-29 12:37:23 +0100

    Fix bug

--- Main/utils.py
+++ Main/utils.py
@@ -3,8 +3,8 @@ import random
 
 def generateId(length=11) :
     chars = string.ascii_letters + string.digits
-    return [random.choice(chars) for _ in range(length)]
+    return ''.join([random.choice(chars) for _ in range(length)])
 
 def generetePlaylistId(length=35) :
     chars = string.ascii_letters + string.digits
-    return [random.choice(chars) for _ in range(length)]
+    return ''.join([random.choice(chars) for _ in range(length)])
