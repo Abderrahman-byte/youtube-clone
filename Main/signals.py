f6727ab54071f3beca592dda41cdc77e860d02ae
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-03 16:43:48 +0100

    Add production media path

--- Main/signals.py
+++ Main/signals.py
@@ -18,25 +18,22 @@ def deletePrevPoster(sender, instance, *args, **kwargs) :
     new_poster = instance.thumbnail_url
 
     if old_poster != new_poster :
-        if settings.DEBUG :
-            old_poster_path = os.path.join(settings.BASE_DIR, old_poster.lstrip('/'))
-            os.remove(old_poster_path)
-        else :
-            # Todo : ADD A WAY TO RETREIVE MEDIA ROOT IN PRODUTION
-            pass
+        poster_path = old_poster.lstrip('/').lstrip('media').lstrip('/')
+        poster_path = os.path.join(settings.MEDIA_ROOT, poster_path)
+        print(poster_path, 'deleted')
+        os.remove(poster_path)
 
 @receiver(pre_delete, sender=Video)
 def deleteMediaOfDeletedVideo(sender, instance, *args, **kwargs) :
     poster_url = instance.thumbnail_url
     video_url = instance.video_url
 
-    if settings.DEBUG :
-        poster_path = os.path.join(settings.BASE_DIR, poster_url.lstrip('/'))
-        video_path = os.path.join(settings.BASE_DIR, video_url.lstrip('/'))
-        os.remove(poster_path)
-        os.remove(video_path)
-
-        print(f'removed {instance.id}')
-    else :
-        # Todo : ADD A WAY TO RETREIVE MEDIA ROOT IN PRODUTION
-        pass
+    
+    poster_path = poster_url.lstrip('/').lstrip('media').lstrip('/')
+    poster_path = os.path.join(settings.MEDIA_ROOT, poster_path)
+    video_path = video_url.lstrip('/').lstrip('media').lstrip('/')
+    video_path = os.path.join(settings.MEDIA_ROOT, video_path)
+    os.remove(poster_path)
+    os.remove(video_path)
+    print(poster_path, 'deleted')
+    print(video_path, 'deleted')
