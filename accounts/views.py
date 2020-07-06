c3fa3ac654bd0b517b4c14ce969085c2135e7be7
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-06 15:28:53 +0100

    Create Login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+class LoginView(View) :
+    def get(self, request) :
+        return render(request, 'accounts/login.html')
