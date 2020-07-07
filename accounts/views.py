1028aaa464ed5059f167e0b1b9ee624509b47926
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-07 17:10:14 +0100

    add post method to Login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.urls import reverse
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 class LoginView(View) :
     def get(self, request) :
-        return render(request, 'accounts/login.html')
+        next = request.GET.get('next')
+        return render(request, 'accounts/login.html', {'next': next})
+
+    def post (self, request) :
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        next = request.POST.get('next')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None :
+            login(request, user)
+            if next is not None :
+                return redirect(next)
+            else :
+                return redirect(reverse('main:index'))
+
+        else :
+            messages.error(request, 'username or password is incorrect')
+            return render(request, 'accounts/login.html', {'next': next})
 
 class RegisterView(View) :
     def get(self, request) :
