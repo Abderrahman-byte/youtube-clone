74866a672982df035480c6200e4f6afb4107eac2
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-09 17:54:32 +0100

    get number of subscriptions

--- channel/views.py
+++ channel/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import Http404
+
 from .models import *
 
 def index(request, id) :
     try :
         channel = Channel.objects.get(pk=id)
-        return render(request, 'channel/index.html', {'channel': channel})
+        subs = channel.user.users.all().count()
+        is_subscribed = request.user in channel.user.users.all()
+        return render(request, 'channel/index.html', {'channel': channel, 'subs': subs, 'is_subscribed': is_subscribed})
     except Channel.DoesNotExist :
         raise Http404
