dfe1b346e994c022b8f8af79f08990f23c0b110e
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-09 15:50:04 +0100

    Create Index view

--- channel/views.py
+++ channel/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import *
 
-# Create your views here.
+def index(request, id) :
+    try :
+        channel = Channel.objects.get(pk=id)
+        return render(request, 'channel/index.html', {'channel': channel})
+    except Channel.DoesNotExist :
+        raise Http404
