3587ba02a07b2095b95fbd59416c54484a4aaebb
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-10 17:01:25 +0100

    Fix is_subscribed Bug

--- channel/views.py
+++ channel/views.py
@@ -7,7 +7,7 @@ def index(request, id) :
     try :
         channel = Channel.objects.get(pk=id)
         subs = channel.user.users.all().count()
-        is_subscribed = request.user in channel.user.users.all()
+        is_subscribed = request.user in [sub.user for sub in channel.user.users.all()]
         return render(request, 'channel/index.html', {'channel': channel, 'subs': subs, 'is_subscribed': is_subscribed})
     except Channel.DoesNotExist :
         raise Http404
