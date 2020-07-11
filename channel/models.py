9078076ce1566923b4385afefac54b1f4d0fa272
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-11 17:50:58 +0100

    Change images fields

--- channel/models.py
+++ channel/models.py
@@ -12,35 +12,35 @@ class Channel(models.Model) :
     title = models.CharField(max_length=200)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     about = models.TextField(max_length=1500, null=True, blank=True)
-    profil_img = models.ImageField(default='images/users/default.png', upload_to=randomFileName)
-    profil_background = models.ImageField(default='images/users/default_background.jpeg', upload_to=randomFileName)
+    profil_img = models.TextField(default='/media/images/users/default.png')
+    profil_background = models.TextField(default='/media/images/users/default_background.jpeg')
 
-    def save(self) :
-        im = Image.open(self.profil_img.path)
-        bg = Image.open(self.profil_background.path)
+    # def save(self) :
+    #     im = Image.open(self.profil_img.path)
+    #     bg = Image.open(self.profil_background.path)
 
-        if str(self.profil_img) != 'images/users/default.png' :
-            im_w, im_h = im.size
+    #     if str(self.profil_img) != 'images/users/default.png' :
+    #         im_w, im_h = im.size
 
-            if im_w > im_h :
-                im = im.crop(((im_w - im_h) / 2 , 0, (im_w + im_h) / 2, im_h))
-            elif im_w < im_h :
-                im = im.crop((0 , (im_h - im_w) / 2 , im_w, (im_h + im_w) / 2))
+    #         if im_w > im_h :
+    #             im = im.crop(((im_w - im_h) / 2 , 0, (im_w + im_h) / 2, im_h))
+    #         elif im_w < im_h :
+    #             im = im.crop((0 , (im_h - im_w) / 2 , im_w, (im_h + im_w) / 2))
 
-            im = im.resize((100, 100))
-            im.save(self.profil_img.path)
+    #         im = im.resize((100, 100))
+    #         im.save(self.profil_img.path)
 
-        if str(self.profil_background) != 'images/users/default_background.jpeg' :
-            bg_w, bg_h = bg.size
+    #     if str(self.profil_background) != 'images/users/default_background.jpeg' :
+    #         bg_w, bg_h = bg.size
 
-            if bg_w / 6.2 < bg_h and bg_h * 6.2 > bg_w :
-                box = (0, (bg_h - bg_w) / 2, bg_w, (bg_h + bg_w) / 2)
-                bg = bg.crop(box)
-            elif bg_w / 6.2 > bg_h and bg_h * 6.2 < bg_w :
-                box = ((bg_w - bg_h) / 2, 0, (bg_w + bg_h) / 2, bg_h)
-                bg = bg.crop(box)
+    #         if bg_w / 6.2 < bg_h and bg_h * 6.2 > bg_w :
+    #             box = (0, (bg_h - bg_w) / 2, bg_w, (bg_h + bg_w) / 2)
+    #             bg = bg.crop(box)
+    #         elif bg_w / 6.2 > bg_h and bg_h * 6.2 < bg_w :
+    #             box = ((bg_w - bg_h) / 2, 0, (bg_w + bg_h) / 2, bg_h)
+    #             bg = bg.crop(box)
 
-            bg = bg.resize((1600, 1600 / 6.2))
-            bg.save(self.profil_background.path)
+    #         bg = bg.resize((1600, 1600 / 6.2))
+    #         bg.save(self.profil_background.path)
 
-        super(Channel, self).save()
+    #     super(Channel, self).save()
