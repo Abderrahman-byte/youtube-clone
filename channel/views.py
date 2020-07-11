965a62c40fd358ca1f831681bdb987bafafbc3d5
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-11 18:14:07 +0100

    Add post method to Modifie Channel View

--- channel/views.py
+++ channel/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.http import Http404
+from django.http import Http404, HttpResponseForbidden
 from django.views.generic import View
+from django.core.files.images import ImageFile
+from django.core.files.storage import FileSystemStorage
 
 from .models import *
+from Main.utils import *
 
 def index(request, id) :
     try :
@@ -25,3 +28,34 @@ class ModifieChannel(View):
             return render(request, 'channel/modifie.html', {'channel': channel})
         else :
             return redirect(reverse('channel:index', args=(channel.id,)))
+
+    def post(self, request, id) :
+        try :
+            channel = Channel.objects.get(pk=id)
+        except :
+            raise Http404
+        
+        if request.user == channel.user :
+            title = request.POST.get('title')
+            about = request.POST.get('about')
+
+            if request.FILES.get('profil-img') is not None :
+                fn, ext = os.path.splitext(request.FILES.get('profil-img').name)
+                fs = FileSystemStorage()
+                profilImg_name = f'images/users/{generateId(30)}{ext}'
+                profilImg = fs.save(profilImg_name, request.FILES.get('profil-img'))
+                channel.profil_img = fs.url(profilImg)
+
+            if request.FILES.get('background-img') is not None :
+                fn, ext = os.path.splitext(request.FILES.get('background-img').name)
+                fs = FileSystemStorage()
+                backgroundImg_name = f'images/users/{generateId(30)}{ext}'
+                backgroundImg = fs.save(backgroundImg_name, request.FILES.get('background-img'))
+                channel.profil_background = fs.url(backgroundImg)
+
+            channel.title = title
+            channel.about = about
+            channel.save()
+            return redirect(reverse('channel:modifie', args=(channel.id,)))
+        else :
+            return HttpResponseForbidden()
