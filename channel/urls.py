7d00065d3abe7febcc041c7b287536ba68d01810
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-11 16:39:05 +0100

    Add ModifieChannel View to urlpatterns

--- channel/urls.py
+++ channel/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'channel'
 
 urlpatterns = [
-    path('<str:id>/', views.index, name='index')
+    path('<str:id>/', views.index, name='index'),
+    path('<str:id>/modifie', views.ModifieChannel.as_view(), name='modifie'),
 ]
