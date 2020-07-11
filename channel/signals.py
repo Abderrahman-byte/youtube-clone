e5cf05a2c996255d1948ee68f6d4d52da93a0ec9
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-11 18:28:05 +0100

    Delete previous profil images on modifie channel

--- channel/signals.py
+++ channel/signals.py
@@ -1,6 +1,9 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.conf import settings
+
+import os
 
 from .models import *
 
@@ -9,3 +12,26 @@ def createChannel(sender, instance, created, *args, **kwargs) :
     if created :
         channel = Channel(title=instance.username, user=instance)
         channel.save()
+
+@receiver(pre_save, sender=Channel)
+def removeUnusedImages(sender, instance, *args, **kwargs) :
+    try :
+        channel = Channel.objects.get(pk=instance.id)
+    except Channel.DoesNotExist :
+        return 
+
+    old_profil = channel.profil_img
+    new_profil = instance.profil_img
+    old_bg = channel.profil_background
+    new_bg = instance.profil_background
+
+    if old_profil != '/media/images/users/default.png' and new_profil != old_profil :
+        old_profil_path = old_profil.lstrip('/').lstrip('media').lstrip('/')
+        old_profil_path = os.path.join(settings.MEDIA_ROOT, old_profil_path)
+        os.remove(old_profil_path)
+
+    if old_bg != '/media/images/users/default_background.jpeg' and new_bg != old_bg :
+        old_bg_path = old_bg.lstrip('/').lstrip('media').lstrip('/')
+        old_bg_path = os.path.join(settings.MEDIA_ROOT, old_bg_path)
+        os.remove(old_bg_path)
+        
