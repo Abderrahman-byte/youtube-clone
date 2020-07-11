f296a8731428e6a1cdb16eabf07c9f7f5ddf7814
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-11 22:24:02 +0100

    Add UserView to accounts urlpatterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login', views.LoginView.as_view(), name='login'),
     path('register', views.RegisterView.as_view(), name='register'),
     path('logout', views.logoutView, name='logout' ),
+    path('user', views.UserView, name='user' ),
 ]
