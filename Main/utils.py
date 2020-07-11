5937b8ec565800f08922f228fde5eec054144e81
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-11 21:52:38 +0100

    Adjust images

--- Main/utils.py
+++ Main/utils.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 import os, string, random, subprocess, math
 import cv2
+from PIL import Image
 
 def generateId(length=11) :
     chars = string.ascii_letters + string.digits
@@ -37,3 +38,25 @@ def randomFileName(instance, filename) :
     id = generateId(30)
     fp, ext = os.path.splitext(filename)
     return f'images/users/{id}{ext}'
+
+def adjustImage(path, w, h) :
+    im = Image.open(path)
+    im_w, im_h = im.size
+    factor = w / h
+
+    if factor == 1 :
+        if im_w > im_h :
+            box = ((im_w - im_h) / 2, 0, (im_w + im_h) / 2, im_h)
+            im = im.crop(box)
+        elif im_w < im_h :
+            box = (0, (im_h - im_w) / 2, im_w, (im_h + im_w) /2)
+            im = im.crop(box)
+    elif im_w / factor > im_h and im_h * factor < im_w :
+        box = ((im_w - (im_h * factor)) / 2, 0, (im_w + (im_h * factor)) / 2, im_h)
+        im = im.crop(box)
+    elif im_w / factor < im_h and im_h * factor > im_w :
+        box = (0, (im_h - (im_w / factor)) / 2, im_w, (im_h + (im_w / factor)) /2)
+        im = im.crop(box)
+    
+    im = im.resize((w, h))
+    im.save(path, quality=1000, subsampling=0)
