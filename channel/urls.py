09ec51855feb5b4822466e75332aa10960d2cbba
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-12 17:38:53 +0100

    add videos View to channel urlpatterns

--- channel/urls.py
+++ channel/urls.py
@@ -7,4 +7,5 @@ app_name = 'channel'
 urlpatterns = [
     path('<str:id>/', views.index, name='index'),
     path('<str:id>/modifie', views.ModifieChannel.as_view(), name='modifie'),
+    path('<str:id>/videos', views.videosView, name='videos'),
 ]
