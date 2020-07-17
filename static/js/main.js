9a15e22295688b0354a5ca4e6c0ac37035da0c9c
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-17 17:05:00 +0100

    move getcookie function from watch file to main

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,22 @@ const navCloser = document.getElementById('hide_nav')
 const nav = document.getElementById('nav')
 const navBars = document.getElementById('nav_bars')
 
+const getCookie = name => {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 const renderTextProperly = () => {
     const textBlock = document.querySelectorAll('.text-block')
 
--- static/js/watch.js
+++ static/js/watch.js
@@ -91,22 +91,6 @@ const updateSuscriptionsCount = () => {
     subscount.textContent = formatCount(count)
 }
 
-const getCookie = name => {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
 const updateViews = () => {
     const views = document.querySelectorAll('.related .video .views')
     views.forEach(viewSpan => {
