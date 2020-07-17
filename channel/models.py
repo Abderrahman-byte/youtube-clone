c86d8cc5f4839acfadbe7a761250a6e90aab11c2
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-17 23:06:12 +0100

    add 'favorite video' field to channel model

--- channel/models.py
+++ channel/models.py
@@ -6,6 +6,7 @@ from PIL import Image
 from io import BytesIO
 
 from Main.utils import *
+from Main.models import *
 
 class Channel(models.Model) :
     id = models.TextField(primary_key=True, max_length=24, default=generateChannelId)
@@ -14,3 +15,10 @@ class Channel(models.Model) :
     about = models.TextField(max_length=1500, null=True, blank=True)
     profil_img = models.TextField(default='/media/images/users/default.png')
     profil_background = models.TextField(default='/media/images/users/default_background.jpeg')
+    favorite_video = models.OneToOneField(Video, on_delete=models.SET_NULL, null=True, blank=True, related_name='favorite_video')
+
+    def save(self, *args, **kwargs) :
+        if (self.favorite_video is not None) and (self.favorite_video.channel.channel.id != self.id) :
+            raise Exception('Favorite video of a channel must be from it.')
+        else :
+            super(Channel).save(self, *args, **kwargs)
